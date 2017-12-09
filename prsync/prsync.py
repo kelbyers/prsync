048f25ee4d334add3c437bdb96bbf66a0292ba87
kel <ruferto@gmail.com>
2017-12-09 16:32:37 -0600

    validate source exists

--- prsync/prsync.py
+++ prsync/prsync.py
@@ -2,13 +2,23 @@
 
 """Main module."""
 
+import os.path
+
 class Prsync:
+    source = None
+
     def __init__(self, src, dst):
         self.init_source(src)
         self.init_destination(dst)
 
+    def run(self):
+        self.validate_source()
+
     def init_source(self, source):
-        pass
+        self.source = source
 
     def init_destination(self, destination):
         pass
+
+    def validate_source(self):
+        os.path.exists(self.source)
--- tests/test_prsync.py
+++ tests/test_prsync.py
@@ -10,13 +10,48 @@ import pytest
 from prsync import prsync
 from prsync import cli
 
+@pytest.fixture(params=['valid', 'invalid'])
+def file_path(request):
+    return request.param
+
 class TestPrsync:
     @patch('prsync.prsync.Prsync.init_source')
     @patch('prsync.prsync.Prsync.init_destination')
-    def test_prsync_class(self, m_init_destination, m_init_source):
+    def test_class(self, m_init_destination, m_init_source):
         from prsync import Prsync
         src = MagicMock()
         dst = MagicMock()
         prsync = Prsync(src=src, dst=dst)
         m_init_source.assert_called_with(src)
         m_init_destination.assert_called_with(dst)
+
+    @patch('os.path')
+    def test_init_source(self, m_os_path):
+        from prsync import Prsync
+        src = MagicMock()
+        dst = MagicMock()
+
+        prsync = Prsync(src=src, dst=dst)
+        assert prsync.source == src
+
+    @patch('prsync.Prsync.validate_source')
+    def test_run_validates_source(self, m_validate_source):
+        from prsync import Prsync
+        src = MagicMock()
+        dst = MagicMock()
+        prsync = Prsync(src=src, dst=dst)
+
+        prsync.run()
+
+        m_validate_source.assert_called_with()
+
+    @patch('os.path.exists')
+    def test_validate_source_valid(self, m_exists):
+        from prsync import Prsync
+        src = MagicMock()
+        dst = MagicMock()
+        prsync = Prsync(src=src, dst=dst)
+
+        prsync.validate_source()
+
+        m_exists.assert_called_with(src)
